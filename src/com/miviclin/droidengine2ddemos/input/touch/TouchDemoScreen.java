8b7bd4859e4807a1589959335b5b82cb23ebd7b9
Miguel Vicente Linares <miviclin@gmail.com>
2014-02-03 00:32:30 +0100

    Change the images used in TouchDemoScreen

--- src/com/miviclin/droidengine2ddemos/input/touch/TouchDemoScreen.java
+++ src/com/miviclin/droidengine2ddemos/input/touch/TouchDemoScreen.java
@@ -1,6 +1,5 @@
 package com.miviclin.droidengine2ddemos.input.touch;
 
-import android.util.Log;
 import android.view.MotionEvent;
 
 import com.miviclin.droidengine2d.Game;
@@ -9,20 +8,17 @@ import com.miviclin.droidengine2d.graphics.Graphics;
 import com.miviclin.droidengine2d.graphics.material.TextureMaterial;
 import com.miviclin.droidengine2d.graphics.texture.TextureAtlas;
 import com.miviclin.droidengine2d.graphics.texture.TexturePackerAtlas;
+import com.miviclin.droidengine2d.graphics.texture.TextureRegion;
 import com.miviclin.droidengine2d.input.TouchInputProcessor;
 import com.miviclin.droidengine2d.screen.Screen;
 import com.miviclin.droidengine2d.util.Transform;
 import com.miviclin.droidengine2d.util.math.Vector2;
-import com.miviclin.droidengine2d.util.time.TimeCounter;
 import com.miviclin.droidengine2ddemos.util.Rectangle;
 
 public class TouchDemoScreen extends Screen {
 
-	private static final int MAX_VISIBLE_TIME_MILLIS = 200;
-
 	private Color backgroundColor;
 	private volatile Rectangle<TextureMaterial> rectangle;
-	private volatile TimeCounter timeCounter;
 
 	public TouchDemoScreen(float viewWidth, float viewHeight, Game game) {
 		super(viewWidth, viewHeight, game);
@@ -30,41 +26,40 @@ public class TouchDemoScreen extends Screen {
 
 	@Override
 	public void update(float delta) {
-		timeCounter.update();
-		getInputManager().processInput();
 	}
 
 	@Override
 	public void draw(Graphics g) {
 		g.setBackgroundColor(backgroundColor);
-		if (timeCounter.getMilliseconds() <= MAX_VISIBLE_TIME_MILLIS) {
-			g.drawRect(rectangle.getMaterial(), rectangle.getTransform());
-		}
+		g.drawRect(rectangle.getMaterial(), rectangle.getTransform());
 	}
 
 	@Override
 	public void onRegister() {
 		backgroundColor = new Color(0.5f, 0.5f, 0.5f);
 
-		TextureAtlas textureAtlas = new TexturePackerAtlas();
-		textureAtlas.loadFromFile("textures/touch-icon-atlas.xml", getGame().getActivity());
+		final TextureAtlas textureAtlas = new TexturePackerAtlas();
+		textureAtlas.loadFromFile("textures/circles.xml", getGame().getActivity());
 		getGame().getTextureManager().addTextureAtlas(textureAtlas);
 
 		Transform transform = new Transform(new Vector2(getWidth() / 2, getHeight() / 2), new Vector2(240, 240));
 		rectangle = new Rectangle<TextureMaterial>(transform,
-				new TextureMaterial(textureAtlas.getTextureRegion("touch-icon.png")));
-
-		timeCounter = new TimeCounter();
+				new TextureMaterial(textureAtlas.getTextureRegion("circle-red.png")));
 
 		getInputManager().getTouchInputController().setTouchInputProcessor(new TouchInputProcessor() {
 
 			@Override
 			public void processMotionEvent(MotionEvent motionEvent) {
+				TextureRegion textureRegion;
 				if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-					rectangle.getTransform().getPosition().set(motionEvent.getX(), getHeight() - motionEvent.getY());
-					timeCounter.reset();
-					Log.d("touch", "touch " + motionEvent.getAction());
+					textureRegion = textureAtlas.getTextureRegion("circle-red.png");
+				} else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+					textureRegion = textureAtlas.getTextureRegion("circle-red.png");
+				} else {
+					textureRegion = textureAtlas.getTextureRegion("circle-blue.png");
 				}
+				rectangle.getMaterial().setTextureRegion(textureRegion);
+				rectangle.getTransform().getPosition().set(motionEvent.getX(), getHeight() - motionEvent.getY());
 			}
 		});
 	}
