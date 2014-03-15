2227fa4d74cfac613d87298928f6e5bfd710be6a
Miguel Vicente Linares <miviclin@gmail.com>
2014-03-15 15:17:00 +0100

    Add Apache License, Version 2.0

--- src/com/miviclin/droidengine2ddemos/gamestate/stacking/BackButtonDialog.java
+++ src/com/miviclin/droidengine2ddemos/gamestate/stacking/BackButtonDialog.java
@@ -1,3 +1,17 @@
+/*   Copyright 2013-2014 Miguel Vicente Linares
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.miviclin.droidengine2ddemos.gamestate.stacking;
 
 import com.miviclin.droidengine2d.graphics.Color;
@@ -37,7 +51,7 @@ public class BackButtonDialog {
 		Vector2 buttonScale = new Vector2(buttonWidth, buttonHeight);
 		this.btnBack.getTransform().getScale().set(buttonScale);
 		this.btnBack.getTransform().getOrigin().set(buttonScale.getX() / 2, buttonScale.getY() / 2);
-		
+
 		float buttonX = dialogPosition.getX();
 		float bottomOfContainer = containerTransform.getPosition().getY() - containerTransform.getOrigin().getY();
 		float buttonY = bottomOfContainer + (buttonScale.getY() / 2) + (buttonMargin / 2);
